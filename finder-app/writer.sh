bec587153cda8c55d8124e8608da71c25242a88b
sohansai <76840110+sohansai@users.noreply.github.com>
2022-11-06 13:53:54 +0530

    Update writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,20 +1,30 @@
-#!/usr/bin/bash
+#!/bin/sh
 
-if [[ $# != 2 ]]; then
-  echo "Something is wrong with the parameters"
-  exit 1
-fi
+# Accepts the following arguments: the first argument is a full path to a file (including filename)
+# on the filesystem, referred to below as writefile; the second argument is a text string which will
+# be written within this file, referred to below as writestr
 
 writefile=$1
 writestr=$2
 
-dname=$(dirname ${writefile})
-fname=$(basename ${writefile})
-
-if ! [[ -d ${filesdir} ]]; then
-  mkdir -p ${dname}
+if [ "$#" -ne 2 ]
+then
+    echo "ERROR: Wrong number of arguments"
+    exit 1
 fi
 
-echo ${writestr} >${writefile}
+path=$(dirname $writefile)
+file=$(basename $writefile)
 
-exit 0
+if [ ! -d $path ]
+then
+    mkdir -p $path
+fi
+
+echo $writestr > $writefile
+
+if [ ! -f $writefile ]
+then 
+    echo "ERROR: Cannot create file"
+    exit 1
+fi
