06c42459ea983aad70e511e717be4c02058d01d9
sohansai <76840110+sohansai@users.noreply.github.com>
2022-11-06 14:07:08 +0530

    Update writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,21 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $# -lt 2 ]
+if [ "$#" -ne 2 ]
 then
-    echo "Invalid number of arguments, expected 2 instead got $# arguments"
-    exit 1
-else
-    writefile=$1
-    writestr=$2
-
-    touch $writefile &> /dev/null
-
-    if [[ $? == "0" ]]
-    then
-        echo $writestr > $writefile
-    else
-        echo "File $writefile could not be created"
-        rm $writefile
-        exit 1
-    fi
+	echo "Expected 2 arguments but got $#"
+	exit 1
 fi
+
+WRITEFILE="$1"
+WRITESTR="$2"
+mkdir -p "${WRITEFILE%/*}"
+echo "$WRITESTR" > "$WRITEFILE"
